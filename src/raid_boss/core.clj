ec5e45ea3036d0e8ced4102169de19ac9a630a8d
Joshua Suskalo <joshua@suskalo.org>
2021-05-28 20:18:08 -0500

    Initial commit

---
+++ src/raid_boss/core.clj
@@ -0,0 +1,5 @@
+(ns raid-boss.core)
+
+(defn -main
+  [& args]
+  (println "Hello, world!"))
---
+++ test/raid_boss/core_test.clj
@@ -0,0 +1,3 @@
+(ns raid-boss.core-test
+  (:require [clojure.test :as t]
+            [raid-boss.core :as sut]))
