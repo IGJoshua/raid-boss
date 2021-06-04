aa033fb61c100334100c2f4e9a20b25a2d0ef776
Joshua Suskalo <joshua@suskalo.org>
2021-06-03 21:14:22 -0500

    Update todos on all events

--- src/raid_boss/events.clj
+++ src/raid_boss/events.clj
@@ -9,39 +9,51 @@
    [raid-boss.components :refer [*db* *gateway* *messaging*]]))
 
 (defn update-guild-state
-  [event-type event-data]
+  [deps event-type event-data]
+  (let [commands (:commands deps)])
   ;; TODO: When a guild is added, the following need to happen
   ;; - Update all the roles list and set them to having permission to ban or not
   ;; - Update the invite counts for the existing invites
+  ;; - Update all the commands for this server
   )
 
 (defn process-new-user
-  [event-type event-data]
-  ;; TODO: When a user joins, we need to get all the invites and see which ones went up
-  ;; Then update the existing invites with the new numbers, and then add the member with the invite
-  ;; If there's only one that went up, just use that invite as the name
-  ;; Otherwise, just don't insert the member item
-
-  ;; If we associate this with one invite, check to see how the invites over the last x minutes
-  ;; compares to the average for this invite over the last y days
-  ;; If it's way higher, then notify the admins and mark it as high risk
-
-  ;; To notify the admins, make a message with a button to mark it as false-positive
-  ;; If false-positive is marked, then remove the high-risk assessment on the item
-  ;; Also add a button to take action which will invalidate the invite and ban everyone from the raid
+  [deps event-type event-data]
+  ;; TODO: When a user joins
+  ;; - Update all the invite counts
+  ;;   - If only one increased, associate that invite with this join event
+  ;;   - Add the user to a recent join-group with this invite, adding it if it doesn't exist
+  ;;   - Calculate if this is a high risk join group and notify admins
+  ;; - Ban the user if their name matches the blacklist
   )
 
 (defn record-unquarantined-user
-  [event-type event-data]
-  ;; TODO: Note that a user has been unbanned or unquarantined
+  [deps event-type event-data]
+  ;; TODO: When a user is updated
+  ;; - If the quarantine role was removed, or the user was unbanned
+  ;;   - Mark the user as having no action taken against them
+  ;;   - Mark action requested and ban appealed to false
+  ;;   - Set the user's ham and spam counts to 0
   )
 
 (defn update-admin-roles
-  [event-type event-data]
-  ;; TODO: Update the role passed to add or remove a role that can use the commands
+  [deps event-type event-data]
+  ;; TODO: When a role is added or updated
+  ;; - If the ban perm is enabled
+  ;;   - Enable the commands
+  ;; - Otherwise
+  ;;   - Disable the commands
+  ;; TODO: When a role is deleted
+  ;; - Remove the role from the cache
   )
 
 (defn record-messages
-  [event-type event-data]
-  ;; TODO: Record whether a given message was ham or spam for a user, maybe banning them
+  [deps event-type event-data]
+  ;; TODO: When a user posts a message
+  ;; - If the message is spam
+  ;;   - Record a spam on the user
+  ;;   - If the user has over 50% spam, take action against them
+  ;;   - If the join group the user is a part of has over 50% spam users, take action against them all
+  ;; - If the message is ham
+  ;;   - Record a ham on the user
   )
